b7af242f7a11116bc2f50078dbd174d90cdd8ba8
Slava <slavbez@mail.ru>
2020-01-10 16:06:20 +0200

    Create object model

--- js/code.js
+++ js/code.js
@@ -1,37 +1,4 @@
-/* let randomLoc = Math.floor(Math.random()*5);
-const location1= randomLoc;
-const location2= 3;
-const location3= 4;
-let guess ="guess";
-let hits = 0;
-let guesses = 0;
-let isSunk = false;
-
-console.log(location1,location2,location3);
-
-while(isSunk == false){
-        guess = prompt("Ready, aim, fire! (enter a number 0-6):");
-    if(guess < 0 || guess > 6){
-        alert("Please enter a valid cell number!");
-    }else{
-        guesses = guesses + 1;
-    }
-
-    if (guess == location1 || guess ==location2 || guess == location3){
-        hits = hits + 1;
-        alert("HIT");
-    } else{
-        alert("MISS");
-    }
-    if(hits === 3 ){
-        isSunk = true;
-        alert("You sank my battleship!");
-    }
-}
-let stats = `You took ${guesses} guesses to sink the battleship, which means your shooting accuracy was ${3/guesses*100}%`;
-alert(stats);
-
- */
+// Обьект представления игры
 const view = {
 
     displayMessage:function(msg) { 
@@ -52,12 +19,71 @@ const view = {
         cell.setAttribute("class","hit");
      }
 }
-
-view.displayMiss("00");
+/* view.displayMiss("00");
 view.displayHit("08");
 view.displayMiss("17");
 view.displayHit("26");
 view.displayMiss("34");
 view.displayHit("42");
 view.displayHit("50");
-view.displayMessage("you fire!");
+view.displayMessage("you fire!"); */
+
+//Обьект МОДЕЛИ
+const model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+
+    ships:[
+        {locations:["00","01","02"],hits:["","",""]},
+        {locations:["11","12","14"],hits:["","",""]},
+        {locations:["47","48","49"],hits:["","",""]},
+    ],
+    fire: function(guess) {
+        //ПЕРЕБЕРАЕМ КОРАБЛИ
+        for(let i =0;i < this.numShips;i++){
+             const ship = this.ships[i];
+             //ПОЛУЧАЕМ МАСИВ КЛЕТОК ЗАНИМАЕМЫХ КОРАБЛЕМ
+             //locations = ship.locations;
+             //ИЩЕМ В МАСИВЕ УКАЗАНОЕ ЗНАЧЕНИЕ 
+             const index = ship.locations.indexOf(guess);
+             if(index >= 0){
+                 //СТАВИМ ОТМЕТКУ hit ПО ТОМУ ЖЕ ИНДЕКСУ
+                 ship.hits[index]= "hit";
+                 //ВЫВОДИМ МАРКЕР ПОПАДАНИЯ В view
+                 view.displayHit(guess);
+                 view.displayMessage("HIT!")
+                 //ПРОВЕРКА НА ПОПАДАНИЯ ЕСЛИ КОРАБЫЛЬ ПОТОПЛИН ТО shipsSunk + 1
+                 if(this.isShunk(ship)){
+                     //СООБЩАЕМ ИГРОКУ ЧТО ОН ПОТАПИЛ КОРАБЫЛЬ
+                     view.displayMessage("You sank my battleship!");
+                     this.shipsSunk++;
+                 }
+                //ЕСЛИ ВЫСТРЕЛ УДАЧНЫЙ
+                 return true;
+
+             }
+        }
+        // ЕСЛИ ВЫСТРЕЛ НЕУДАЧНЫЙ
+        view.displayMiss(guess);
+        view.displayMessage("You missed.");
+        return false;
+    },
+
+    //ПРОВЕРЯЕМ ПОТОПЛЕН ЛИ КОРАБЫЛЬ
+    isShunk:function(ship) {
+        for(let i =0; i < this.shipLength;i++){
+            if(ship.hits[i] !== "hit"){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
+
+
+
+
+
